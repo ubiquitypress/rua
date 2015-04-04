6f288c4a132c055e82d2bedd73b25982e7501250
Andy Byers <andy.byers@ubiquitypress.com>
2015-04-04 13:18:14 -0400

    Added login and logout stuff.

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +11,15 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'core.views.index', name='index'),
+    url(r'^contact/$', 'core.views.contact', name='contact'),
+    url(r'^login/$', 'core.views.login', name='login'),
+    url(r'^logout/$', 'core.views.logout', name='logout'),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('', 
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500/$', TemplateView.as_view(template_name='500.html')),
+    )
--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout as logout_user
+from django.contrib.auth import login as login_user
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+# Website Views
 
 def index(request):
 
@@ -6,3 +14,61 @@ def index(request):
 	context = {}
 
 	return render(request, template, context)
+
+def contact(request):
+
+	template = "core/contact.html"
+	context = {}
+
+	return render(request, template, context)
+
+# Authentication Views
+
+def login(request):
+	if request.user.is_authenticated():
+		messages.info(request, 'You are already logged in.')
+		return redirect(reverse('monitor_dashboard'))
+
+	if request.POST:
+		user = request.POST.get('user_name')
+		pawd = request.POST.get('user_pass')
+
+		print user, pawd
+
+		user = authenticate(username=user, password=pawd)
+
+		if user is not None:
+			if user.is_active:
+				login_user(request, user)
+				messages.info(request, 'Login successful.')
+				if request.GET.get('next'):
+					return redirect(request.GET.get('next'))
+				else:
+					return redirect(reverse('index'))
+			else:
+				messages.add_message(request, messages.ERROR, 'User account is not active.')
+		else:
+			messages.add_message(request, messages.ERROR, 'Account not found with those details.')
+
+	context = {}
+	template = 'core/login.html'
+
+	return render(request, template, context)
+
+def logout(request):
+	messages.info(request, 'You have been logged out.')
+	logout_user(request)
+	return redirect(reverse('index'))
+
+def register(request):
+	pass
+
+def register_complete(request):
+	pass
+
+def reset_password(request):
+	pass
+
+@login_required
+def admintest(request):
+	pass
