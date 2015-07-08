49c5cd4449e7688e6b3b3a18f2eaeb35dd0cdf0b
Andy Byers <andy.byers@ubiquitypress.com>
2015-07-08 22:00:50 +0100

    migration merge.

---
+++ src/core/migrations/0046_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0044_auto_20150708_1956'),
+        ('core', '0045_reviewassignment_review_round'),
+    ]
+
+    operations = [
+    ]
