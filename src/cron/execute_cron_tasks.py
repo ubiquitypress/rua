43fc26ae01a26477b52e74bcb1c1a0f2631d4bb9
Andy Byers <andy.byers@ubiquitypress.com>
2015-08-24 11:49:57 +0100

    Updated

--- src/cron/execute_cron_tasks.py
+++ src/cron/execute_cron_tasks.py
@@ -9,14 +9,11 @@ def remind_unaccepted_reviews(task):
 	days = int(models.Setting.objects.get(group__name='cron', name='remind_unaccepted_reviews').value)
 	dt = timezone.now()
 	target_date = dt + timedelta(days=days)
+	print target_date
 
 	books = models.Book.objects.filter(stage__current_stage='review')
 
-	print books
-	print target_date.date()
-
 	for book in books:
-		print book.get_latest_review_round()
 		reviews = models.ReviewAssignment.objects.filter(book=book, review_round__round_number=book.get_latest_review_round(), accepted__isnull=True, declined__isnull=True, due=target_date.date)
 		pprint(reviews)
 
