1b772f96b78a9c4bbe0813c22c843025bec9be12
Andy Byers <andy.byers@ubiquitypress.com>
2015-08-27 13:54:06 +0100

    Adding function for editor to view revision pt 1.

--- src/revisions/views.py
+++ src/revisions/views.py
@@ -78,6 +78,10 @@ def revision(request, revision_id):
 
 	return render(request, template, context)
 
+@staff_member_required
+def editor_view_revisions(request, revision_id):
+	pass
+
 @login_required
 def update_file(request, revision_id, file_id):
 	revision = get_object_or_404(models.Revision, pk=revision_id, book__owner=request.user)
