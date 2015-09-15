808bed9e37d5c29fc4a4af193c8ed0fa7e6300f1
Andy Byers <andy.byers@ubiquitypress.com>
2015-09-15 21:41:16 +0100

    Updated some fixtures.

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -1,6 +1,8 @@
 from core import logic
 from core.cache import cache_result
 
+from django.contrib.auth.models import Group
+
 @cache_result(300)
 def press(request):
 	return {'press': logic.press_settings()}
@@ -10,3 +12,7 @@ def task_count(request):
 		return {'task_count': logic.task_count(request)}
 	except:
 		return 0
+
+def user_groups(request):
+	return {'groups': [group.name for group in request.user.groups_set.all()]}
+
