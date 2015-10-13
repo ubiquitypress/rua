ac1138dc77a027f299398b795c7ef443d310d4ef
Andy Byers <andy.byers@ubiquitypress.com>
2015-10-13 16:43:59 +0100

    test now uses mysql.

--- src/core/test_settings.py
+++ src/core/test_settings.py
@@ -87,10 +87,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rua_test',
+        'USER': 'rua_test',
+        'PASSWORD': 'hySPVknVSJvJHkR',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
