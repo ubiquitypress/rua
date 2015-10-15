b6d8a2971ea7b42483ad2058fcebbf44a01ab46e
Andy Byers <andy.byers@ubiquitypress.com>
2015-10-15 13:19:57 +0100

    Updated returns to dicts.

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -13,23 +13,23 @@ def task_count(request):
 	try:
 		return {'task_count': logic.task_count(request)}
 	except:
-		return 0
+		return {'task_count': 0}
 def review_assignment_count(request):
 	try:
 		return {'review_assignment_count': logic.review_assignment_count(request)}
 	except:
-		return 0
+		return {'review_assignment_count': 0}
 
 def onetasker_task_count(request):
 	try:
 		return {'onetasker_task_count':  logic.onetasker_task_count(request)}
 	except:
-		return 0
+		return {'onetasker_task_count':  0}
 def author_task_count(request):
 	try:
 		return {'author_task_count':  len(author_tasks(request.user))}
 	except:
-		return 0
+		return {'author_task_count':  0}
 
 def roles(request):
 	try:
