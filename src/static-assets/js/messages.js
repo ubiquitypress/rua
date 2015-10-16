802558b41b63c0eabc8371023b262fa83f512a6e
Andy Byers <andy.byers@ubiquitypress.com>
2015-10-16 13:27:12 +0100

    Updated messages system to use correct DT format. Also set it JS to use the users actual initials.

--- src/static-assets/js/messages.js
+++ src/static-assets/js/messages.js
@@ -13,7 +13,7 @@ frm.submit(function () {
                 var style = 'odd'
             }
 
-            $( "#message-list" ).prepend( '<div class="message ' + style + '"><div class="row message-container"><div class="col-md-2"><h3 style="margin-top: 15px;">AB</h3></div><div class="col-md-10"><small>' + data.message + '</small></div><div class="col-md-12 message-date"><small>' + data.date_sent + '</small></div></div></div>' );
+            $( "#message-list" ).prepend( '<div class="message ' + style + '"><div class="row message-container"><div class="col-md-2"><h3 style="margin-top: 15px;">' + $('#initials').val()  + '</h3></div><div class="col-md-10"><small>' + data.message + '</small></div><div class="col-md-12 message-date"><small>' + data.date_sent + '</small></div></div></div>' );
             $('#id_message').val("");
         },
         error: function(data) {
