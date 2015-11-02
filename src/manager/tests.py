62c984b9a81230ba437894a31e0890034bf73dad
Ioannis Cleary <ioannis.cleary@ubiquitypress.com>
2015-11-02 15:14:01 +0000

    Started test cases for Manager app

--- src/manager/tests.py
+++ src/manager/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
-
+from onetasker import models
+from django.utils import timezone
+import time
+import datetime
+from django.test import SimpleTestCase
+from django.db.models import Q
+from onetasker import views
+from core import models as core_models
+from core import logic as core_logic, task
+import json
+from django.http import HttpRequest
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import resolve, reverse
+from  __builtin__ import any as string_any
+import calendar
 # Create your tests here.
+
+class CoreTests(TestCase):
+
+	# Dummy DBs
+	fixtures = [
+		'settinggroups',
+		'settings',
+		'langs',
+		'cc-licenses',
+		'role',
+		'test_auth_data',
+		'test_review_data',
+		'test_core_data',
+		'test_index_assignment_data',
+		'test_copyedit_assignment_data',
+
+	]
+
+	def setUp(self):
+		self.client = Client(HTTP_HOST="testing")
+		self.user = User.objects.get(username="rua_user")
+		self.user.save()
+		self.book = core_models.Book.objects.get(pk=1)
+
+
+		login = self.client.login(username="rua_user", password="root")
+		self.assertEqual(login, True)
+
+	def tearDown(self):
+		pass
+
+	def test_set_up(self):
+		"""
+		testing set up
+		"""
+		self.assertEqual(self.user.username=="rua_user", True)
+		self.assertEqual(self.user.email=="fake_user@fakeaddress.com", True)
+		self.assertEqual(self.user.first_name=="rua_user_first_name", True)
+		self.assertEqual(self.user.last_name=="rua_user_last_name", True)
+		self.assertEqual(self.user.profile.institution=="rua_testing", True)
+		self.assertEqual(self.user.profile.country=="GB", True)
+
+	def test_manager_access_staff(self):
+		resp = self.client.get(reverse('manager_index'))
+		content =resp.content
+		
+		self.assertEqual(resp.status_code, 200)
+		self.assertEqual("403" in content, False)
+
+	def test_manager_access_not_staff(self):
+		login = self.client.login(username="rua_reviewer", password="tester")
+		resp = self.client.get(reverse('manager_index'))
+		
+		self.assertEqual(resp.status_code, 302)
+		self.assertEqual(resp['Location'], "http://testing/admin/login/?next=/manager/")
