3e08c6d6f1f3c4c0929284db7ae7ed794985be27
Ioannis Cleary <ioannis.cleary@ubiquitypress.com>
2015-11-05 10:29:57 +0000

    added django-coverage app to test coverage

--- src/core/dev_settings.py
+++ src/core/dev_settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+
 
     # Django
     'core',
@@ -210,3 +212,18 @@ AUTHENTICATION_BACKENDS = (
 SILENCED_SYSTEM_CHECKS = (
     'fields.W340',
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+       '--with-coverage',  # activate coverage report
+        '--with-doctest',  # activate doctest: find and run docstests
+        '--verbosity=2',   # verbose output 
+        '--with-xunit',    # enable XUnit plugin
+        '--xunit-file=xunittest.xml',  # the XUnit report file
+        '--cover-xml',     # produle XML coverage info
+        '--cover-xml-file=coverage.xml',  # the coverage info file
+        '--cover-package=core,author,editor,manager,onetasker,review,submission'
+        # You may also specify the packages to be covered here
+        # '--cover-package=blog,examples'
+]
