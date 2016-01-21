a8b80795cf33fc986d6308e77ce34a5e3b5e4e34
Ioannis Cleary <ioannis.cleary@ubiquitypress.com>
2016-01-21 13:34:05 +0000

    moved js to shared file for bottom-space browser check + added if not the 5 browsers then load the height version

---
+++ src/static-assets/js/bottom-space-check.js
@@ -0,0 +1,96 @@
+var path = padding
+var path2 = nopadding
+
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
+
+if (isFirefox) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path2;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
+if (isIE) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path2;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
+if (isOpera) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path2;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
+if (isChrome) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
+
+if (isSafari) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
+if (!isFirefox && !isSafari && !isChrome && !isIE && !isOpera) {
+	var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path2;
+    link.media = 'all';
+    head.appendChild(link);
+}
+}
