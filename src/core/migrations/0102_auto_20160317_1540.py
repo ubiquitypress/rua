76f76a1f9b3c97fa43a9ef093fc25a71faeb7f81
IoannisCleary <nicolemaniac@gmail.com>
2016-03-17 15:40:38 +0000

    Updated editorial review assignment model

---
+++ src/core/migrations/0102_auto_20160317_1540.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('review', '0006_auto_20160120_1640'),
+        ('core', '0101_auto_20160317_1454'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='editorialreviewassignment',
+            old_name='competing_interests',
+            new_name='editorial_board_competing_interests',
+        ),
+        migrations.RenameField(
+            model_name='editorialreviewassignment',
+            old_name='recommendation',
+            new_name='editorial_board_recommendation',
+        ),
+        migrations.RemoveField(
+            model_name='editorialreviewassignment',
+            name='results',
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='editorial_board_results',
+            field=models.ForeignKey(related_name='eb_review_form_results', blank=True, to='review.FormResult', null=True),
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='editorial_board_review_form',
+            field=models.ForeignKey(related_name='eb_review_form', blank=True, to='review.Form', null=True),
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='publication_committee_competing_interests',
+            field=models.TextField(help_text=b"If any of the authors or editors have any competing interests please add them here. EG. 'This study was paid for by corp xyz.'. <a href='/page/competing_interests/'>More info</a>", null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='publication_committee_recommendation',
+            field=models.CharField(blank=True, max_length=10, null=True, choices=[(b'accept', b'Accept'), (b'reject', b'Reject'), (b'revisions', b'Revisions Required')]),
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='publication_committee_results',
+            field=models.ForeignKey(related_name='pc_review_form_results', blank=True, to='review.FormResult', null=True),
+        ),
+        migrations.AddField(
+            model_name='editorialreviewassignment',
+            name='publication_committee_review_form',
+            field=models.ForeignKey(related_name='pc_review_form', blank=True, to='review.Form', null=True),
+        ),
+    ]
--- src/core/models.py
+++ src/core/models.py
@@ -459,25 +459,41 @@ class ReviewAssignment(models.Model):
 		return u'%s - %s %s' %  (self.pk, self.book.title, self.user.username)
 
 class EditorialReviewAssignment(models.Model):
+	
 	book = models.ForeignKey(Book) #TODO: Remove this as it is already linked to the book through the review round
+	
 	assigned = models.DateField(auto_now=True)
 	accepted = models.DateField(blank=True, null=True)
 	declined = models.DateField(blank=True, null=True)
 	due = models.DateField(blank=True, null=True)
 	completed = models.DateField(blank=True, null=True)
+	
 	files = models.ManyToManyField('File', blank=True, null=True)
 	body = models.TextField(blank=True, null=True)
+	
 	#Special fields
+	
 	editorial_board = models.ManyToManyField(User, blank=True, null=True)
 	management_editor = models.ForeignKey(User, related_name='management_editor')
+	
 	editorial_board_access_key = models.CharField(max_length=258,null=True,blank=True)
 	publishing_committee_access_key = models.CharField(max_length=258,null=True,blank=True)
+	
 	editorial_board_passed = models.BooleanField(default=False)
 	publication_committee_passed = models.BooleanField(default=False)
 
-	results = models.ForeignKey('review.FormResult', null=True, blank=True)
-	recommendation = models.CharField(max_length=10, choices=review_recommendation(), null=True, blank=True)
-	competing_interests = models.TextField(blank=True, null=True, help_text=mark_safe("If any of the authors or editors have any competing interests please add them here. EG. 'This study was paid for by corp xyz.'. <a href='/page/competing_interests/'>More info</a>"))
+	editorial_board_results = models.ForeignKey('review.FormResult', null=True, blank=True, related_name="eb_review_form_results")
+	publication_committee_results = models.ForeignKey('review.FormResult', null=True, blank=True,related_name="pc_review_form_results")
+	
+	editorial_board_recommendation = models.CharField(max_length=10, choices=review_recommendation(), null=True, blank=True)
+	editorial_board_competing_interests = models.TextField(blank=True, null=True, help_text=mark_safe("If any of the authors or editors have any competing interests please add them here. EG. 'This study was paid for by corp xyz.'. <a href='/page/competing_interests/'>More info</a>"))
+	
+	publication_committee_recommendation = models.CharField(max_length=10, choices=review_recommendation(), null=True, blank=True)
+	publication_committee_competing_interests = models.TextField(blank=True, null=True, help_text=mark_safe("If any of the authors or editors have any competing interests please add them here. EG. 'This study was paid for by corp xyz.'. <a href='/page/competing_interests/'>More info</a>"))
+
+	editorial_board_review_form = models.ForeignKey('review.Form', null=True, blank=True,related_name="eb_review_form")
+	publication_committee_review_form = models.ForeignKey('review.Form', null=True, blank=True,related_name="pc_review_form")
+
 
 	# Used to ensure that an email is not sent more than once.
 	unaccepted_reminder = models.BooleanField(default=False)
