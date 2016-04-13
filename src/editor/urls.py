d16794742cfbced03e5649456b63a6264aa0afc7
Andy Byers <andy.byers@ubiquitypress.com>
2016-04-13 18:15:19 +0100

    Tidy up editor urls.py

--- src/editor/urls.py
+++ src/editor/urls.py
@@ -34,6 +34,7 @@ urlpatterns = patterns('',
     url(r'^review/submission/(?P<submission_id>\d+)/assignment/(?P<review_id>\d+)/set/due/$', 'editor.views.update_editorial_review_due_date', name='update_editorial_review_due_date'),
     url(r'^review/submission/(?P<submission_id>\d+)/assignment/(?P<review_id>\d+)/decision/(?P<decision>[-\w]+)/$', 'editor.views.editor_editorial_decision', name='editor_editorial_decision'),
 
+    # Review
     url(r'^submission/(?P<submission_id>\d+)/review/round/(?P<round_id>\d+)/assignment/(?P<review_id>\d+)/$', 'editor.views.editor_review_assignment', name='editor_review_assignment'),
     url(r'^submission/(?P<submission_id>\d+)/review/round/(?P<round_id>\d+)/assignment/(?P<review_id>\d+)/set/due/$', 'editor.views.update_review_due_date', name='update_review_due_date'),
     url(r'^submission/(?P<submission_id>\d+)/files/(?P<review_type>[-\w]+)/add/$', 'editor.views.add_review_files', name='add_review_files'),
@@ -42,6 +43,7 @@ urlpatterns = patterns('',
     url(r'^submission/submission/(?P<submission_id>\d+)/revisions/request/returner/(?P<returner>[-\w]+)/$', 'editor.views.request_revisions', name='request_revisions'),
     url(r'^submission/submission/(?P<submission_id>\d+)/revisions/view/(?P<revision_id>\d+)/$', 'editor.views.editor_view_revisions', name='editor_view_revisions'),
 
+    # Editing
     url(r'^submission/(?P<submission_id>\d+)/editing/$', 'editor.views.editor_editing', name='editor_editing'),
     url(r'^submission/(?P<submission_id>\d+)/editing/assign/copyeditor/$', 'editor.views.assign_copyeditor', name='assign_copyeditor'),
     url(r'^submission/(?P<submission_id>\d+)/editing/view/copyeditor/(?P<copyedit_id>\d+)/$', 'editor.views.view_copyedit', name='view_copyedit'),
@@ -49,28 +51,29 @@ urlpatterns = patterns('',
     url(r'^submission/(?P<submission_id>\d+)/editing/assign/indexer/$', 'editor.views.assign_indexer', name='assign_indexer'),
     url(r'^submission/(?P<submission_id>\d+)/editing/view/indexer/(?P<index_id>\d+)/$', 'editor.views.view_index', name='view_index'),
 
-
-    url(r'^submission/(?P<submission_id>\d+)/production/$', 'editor.views.editor_production', name='editor_production'),
+    # Production
     url(r'^submission/(?P<submission_id>\d+)/publish/$', 'editor.views.editor_publish', name='editor_publish'),
-
+    url(r'^submission/(?P<submission_id>\d+)/production/$', 'editor.views.editor_production', name='editor_production'),
     url(r'^submission/(?P<submission_id>\d+)/production/add/format/$', 'editor.views.add_format', name='add_format'),
     url(r'^submission/(?P<submission_id>\d+)/production/add/chapter/$', 'editor.views.add_chapter', name='add_chapter'),
     url(r'^submission/(?P<submission_id>\d+)/production/chapter/(?P<chapter_id>\d+)/add/format/$', 'editor.views.add_chapter_format', name='add_chapter_format'),
     url(r'^submission/(?P<submission_id>\d+)/production/chapter/(?P<chapter_id>\d+)/view/$', 'editor.views.view_chapter', name='editor_view_chapter'),
     url(r'^submission/(?P<submission_id>\d+)/production/chapter/(?P<chapter_id>\d+)/update/$', 'editor.views.update_chapter', name='editor_update_chapter'),
     url(r'^submission/(?P<submission_id>\d+)/production/chapter/(?P<chapter_id>\d+)/view/format/(?P<format_id>\d+)/$', 'editor.views.view_chapter_format', name='editor_view_chapter_format'),
-    
     url(r'^submission/(?P<submission_id>\d+)/production/add/physical/$', 'editor.views.add_physical', name='add_physical'),
     url(r'^submission/(?P<submission_id>\d+)/production/add/format/(?P<file_id>\d+)/$', 'editor.views.add_format', name='add_format_existing'),
     url(r'^submission/(?P<submission_id>\d+)/production/add/chapter/(?P<chapter_id>\d+)/file/(?P<file_id>\d+)/$', 'editor.views.add_chapter', name='add_chapter_existing'),
-
     url(r'^submission/(?P<submission_id>\d+)/production/delete/(?P<format_or_chapter>[-\w]+)/(?P<id>\d+)/$', 'editor.views.delete_format_or_chapter', name='delete_format_or_chapter'),
     url(r'^submission/(?P<submission_id>\d+)/production/update/(?P<format_or_chapter>[-\w]+)/(?P<id>\d+)/$', 'editor.views.update_format_or_chapter', name='update_format_or_chapter'),
-   
+    url(r'^submission/(?P<submission_id>\d+)/production/assign/typesetter/$', 'editor.views.assign_typesetter', name='assign_typesetter'),
+    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/$', 'editor.views.view_typesetter', name='view_typesetter'),
+    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/alter/due-date/$', 'editor.views.view_typesetter_alter_due_date', name='view_typesetter_alter_due_date'),
+    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/alter/author-due/$', 'editor.views.view_typesetter_alter_author_due', name='view_typesetter_alter_author_due'),
+
+    # Catalog
     url(r'^submission/(?P<submission_id>\d+)/catalog/$', 'editor.views.catalog', name='catalog'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/marc21/$', 'editor.views.catalog_marc21', name='catalog_marc21'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/marc21/load/(?P<type>[-\w]+)$', 'editor.views.catalog_marc21', name='catalog_marc21_load'),
-   
     url(r'^submission/(?P<submission_id>\d+)/catalog/identifiers/$', 'editor.views.identifiers', name='identifiers'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/identifiers/(?P<identifier_id>\d+)/$', 'editor.views.identifiers', name='identifiers_with_id'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/retailers/$', 'editor.views.retailers', name='retailers'),
@@ -78,11 +81,7 @@ urlpatterns = patterns('',
     url(r'^submission/(?P<submission_id>\d+)/catalog/contributor/(?P<contributor_type>[-\w]+)/(?P<contributor_id>\d+)/$', 'editor.views.update_contributor', name='update_contributor'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/contributor/(?P<contributor_type>[-\w]+)/$', 'editor.views.update_contributor', name='add_contributor'),
     url(r'^submission/(?P<submission_id>\d+)/catalog/contributor/(?P<contributor_type>[-\w]+)/(?P<contributor_id>\d+)/delete/$', 'editor.views.delete_contributor', name='delete_contributor'),
-    url(r'^submission/(?P<submission_id>\d+)/production/assign/typesetter/$', 'editor.views.assign_typesetter', name='assign_typesetter'),
-    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/$', 'editor.views.view_typesetter', name='view_typesetter'),
-    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/alter/due-date/$', 'editor.views.view_typesetter_alter_due_date', name='view_typesetter_alter_due_date'),
-    url(r'^submission/(?P<submission_id>\d+)/production/view/typesetter/(?P<typeset_id>\d+)/alter/author-due/$', 'editor.views.view_typesetter_alter_author_due', name='view_typesetter_alter_author_due'),
-
+   
     # Contract
     url(r'^contract/(?P<submission_id>\d+)/manage/$', 'editor.views.contract_manager', name='contract_manager'),
     url(r'^contract/(?P<submission_id>\d+)/manage/(?P<contract_id>\d+)/$', 'editor.views.contract_manager', name='contract_manager_edit'),
