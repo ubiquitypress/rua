08f4a4d4ecb80476a0c1637e2af07f96cf22d9f7
IoannisCleary <nicolemaniac@gmail.com>
2016-04-25 15:58:58 +0100

    Closes #426 updated other pages with suggested fixes

---
+++ src/core/migrations/0120_auto_20160425_1455.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0119_auto_20160425_1455'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='book',
+            name='description',
+            field=models.TextField(help_text=b'This is used for metadata, the website text and the back cover of the book', max_length=5000, null=True, verbose_name=b'Abstract', blank=True),
+        ),
+    ]
