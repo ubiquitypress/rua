abc066acfa62c8f1ed64db5db5196114af597231
Andy Byers <andy.byers@ubiquitypress.com>
2016-06-21 11:39:34 +0100

    no message

--- src/core/migrations/0126_auto_20160505_1228.py
+++ src/core/migrations/0126_auto_20160505_1228.py
@@ -8,7 +8,7 @@ from core import models as core_models
 def load_setting(apps, schema_editor):
     # We can't import the Person model directly as it may be a newer
     # version than this migration expects. We use the historical version.
-    setting_group = core_models.SettingGroup.objects.get_or_create(name='email_subject', defaults={'enabled': True})
+    setting_group, created = core_models.SettingGroup.objects.get_or_create(name='email_subject', defaults={'enabled': True})
     setting_subject = core_models.Setting.objects.get_or_create(name='change_principal_contact_proposal_subject', group=setting_group, types ='text', defaults={ 'value' : 'Proposal Owner Change', 'description' : "Email subject. Do not remove '%s' or other string format related parts." })
 
 
