fe36a1ba1630ef63303a5195b55276cf1cbf7f8f
stuartjennings-up <stuart.jennings@ubiquitypress.com>
2016-09-06 16:15:55 +0100

    fix css override that meant panels didn't show

--- src/core/views.py
+++ src/core/views.py
@@ -1831,11 +1831,11 @@ def proposal_history(request):
 def view_proposal(request, proposal_id):
     proposal = get_object_or_404(submission_models.Proposal, pk=proposal_id)
     relationships = models.ProposalFormElementsRelationship.objects.filter(form=proposal.form)
+
     if proposal.data:
         data = json.loads(proposal.data)
     else:
         data = {}
-
     if not request.POST and request.GET.get('download') == 'docx':
         path = create_proposal_form(proposal)
         return serve_proposal_file(request, path)
