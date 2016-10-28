459044a8a3ffe12a60af034ace1052bd3d32cdc2
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2016-10-28 16:45:09 +0100

    apply mark_safe to all lookup tags

--- src/core/templatetags/dict_lookup.py
+++ src/core/templatetags/dict_lookup.py
@@ -9,20 +9,11 @@ import os
 
 @register.simple_tag
 def lookup(the_dict, key):
-	# Try to fetch from the dict, and if it's not found return an empty string.
-	the_dict = json.loads(the_dict)
-	if the_dict.get(key, '')[1] == 'upload':
-		url = reverse('proposals:serve_file', kwargs={'filename': os.path.basename(the_dict.get(key, '')[0])})
-   		return '<a href="%s">Download File</a>' % url
-   	else:
-   		return linebreaksbr(the_dict.get(key, '')[0])
-
-@register.simple_tag
-def lookup_safe(the_dict, key):
-	# Try to fetch from the dict, and if it's not found return an empty string. Output marked safe from HTML escape.
+	# Try to fetch from the dict, and if it's not found return an empty string, with output safe from HTML escaping.
 	the_dict = json.loads(the_dict)
 	if the_dict.get(key, '')[1] == 'upload':
 		url = reverse('proposals:serve_file', kwargs={'filename': os.path.basename(the_dict.get(key, '')[0])})
    		return '<a href="%s">Download File</a>' % url
    	else:
    		return linebreaksbr(mark_safe(the_dict.get(key, '')[0]))
+
