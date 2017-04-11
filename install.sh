8937b14156269cd24eda73b588859f2e5ef9723d
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2017-04-11 13:52:48 +0100

    restore setting override and re-order install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e # everything must succeed.
 
+
+if [ ! -z src/core/settings.py ]; then
+    cd src/core/
+    ln -sf dev_settings.py settings.py
+    cd -
+fi
+
 # if virtual environment called 'venv' doesn't exist, create it
 if [ ! -d venv ]; then
     virtualenv --python=`which python2` venv
@@ -8,12 +15,6 @@ fi
 
 # activate it
 source venv/bin/activate
-
-if [ ! -z src/core/settings.py ]; then
-    cd src/core/
-    ln -s dev_settings.py settings.py
-    cd -
-fi
 
 # install any requirements
 pip install -r requirements.txt
