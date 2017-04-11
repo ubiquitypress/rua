ff7b7ce86d82cd9b6ef91a9d83f9b2d9ab9b1a49
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2017-04-11 14:33:18 +0100

    more tinkering trying to get travis to work

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e # everything must succeed.
 
-echo "install"
-
 # if virtual environment called 'venv' doesn't exist, create it
 if [ ! -d venv ]; then
     virtualenv --python=`which python2` venv
@@ -11,19 +9,13 @@ fi
 # activate it
 source venv/bin/activate
 
-echo "env activated"
-
 if [ ! -z src/core/settings.py ]; then
     cd src/core/
     ln -sf dev_settings.py settings.py
     cd -
 fi
 
-echo "settings done"
-
 # install any requirements
 pip install -r requirements.txt
-
-echo "requirements installed"
 
 source .configure.sh
