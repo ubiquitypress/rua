3f624486df1d2463ad5aa4634f58b25d982bf257
Francesco de Virgilio <fradeve@inventati.org>
2017-12-14 10:52:55 +0000

    PEP8

--- src/manager/forms.py
+++ src/manager/forms.py
@@ -1,38 +1,44 @@
 from django import forms
-from django_summernote.widgets import SummernoteWidget
+from django.template.defaultfilters import slugify
 from django.utils.encoding import smart_text
 
-from django.template.defaultfilters import slugify
-from manager import models
+from django_summernote.widgets import SummernoteWidget
+
 from core import models as core_models
+from manager import models
 from review import models as review_models
 
 
 class GroupForm(forms.ModelForm):
+
     class Meta:
         model = models.Group
         exclude = ()
 
 
 class SeriesForm(forms.ModelForm):
+
     class Meta:
         model = core_models.Series
         exclude = ()
 
 
 class FormElement(forms.ModelForm):
+
     class Meta:
         model = review_models.FormElement
         exclude = ('form',)
 
 
 class FormElementsRelationship(forms.ModelForm):
+
     class Meta:
         model = review_models.FormElementsRelationship
         exclude = ('form', 'element')
 
 
 class ProposalForms(forms.ModelForm):
+
     class Meta:
         model = core_models.ProposalForm
         exclude = ('proposal_fields',)
@@ -43,18 +49,21 @@ class ProposalForms(forms.ModelForm):
 
 
 class ProposalElement(forms.ModelForm):
+
     class Meta:
         model = core_models.ProposalFormElement
         exclude = ('form',)
 
 
 class ProposalElementRelationship(forms.ModelForm):
+
     class Meta:
         model = core_models.ProposalFormElementsRelationship
         exclude = ('form', 'element')
 
 
 class ReviewForm(forms.ModelForm):
+
     class Meta:
         model = review_models.Form
         exclude = ('form_fields',)
@@ -65,6 +74,7 @@ class ReviewForm(forms.ModelForm):
 
 
 class EditKey(forms.Form):
+
     def __init__(self, *args, **kwargs):
         key_type = smart_text(kwargs.pop('key_type', None))
         value = kwargs.pop('value', None)
@@ -90,36 +100,70 @@ class EditKey(forms.Form):
 
 
 class ProposalForm(forms.Form):
+
     def __init__(self, *args, **kwargs):
         super(ProposalForm, self).__init__(*args, **kwargs)
-        self.fields['selection'].choices = [(proposal_form.pk, proposal_form)
-                                            for proposal_form in
-                                            core_models.ProposalForm.objects.all()]
+        self.fields['selection'].choices = [
+            (proposal_form.pk, proposal_form)
+            for proposal_form in core_models.ProposalForm.objects.all()
+        ]
 
     selection = forms.ChoiceField(widget=forms.Select)
 
 
 class DefaultReviewForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput, required=True)
-    ref = forms.CharField(widget=forms.TextInput, required=True)
-    intro_text = forms.CharField(widget=forms.Textarea, required=True)
-    completion_text = forms.CharField(widget=forms.Textarea, required=True)
+
+    name = forms.CharField(
+        widget=forms.TextInput,
+        required=True,
+    )
+    ref = forms.CharField(
+        widget=forms.TextInput,
+        required=True,
+    )
+    intro_text = forms.CharField(
+        widget=forms.Textarea,
+        required=True,
+    )
+    completion_text = forms.CharField(
+        widget=forms.Textarea,
+        required=True,
+    )
 
 
 class DefaultForm(forms.Form):
-    title = forms.CharField(widget=forms.TextInput, required=True,
-                            label='Book Title')
-    subtitle = forms.CharField(widget=forms.TextInput, required=False)
-    author = forms.CharField(widget=forms.TextInput, required=True,
-                             label='Submitting Author/Editor')
+
+    title = forms.CharField(
+        widget=forms.TextInput,
+        required=True,
+        label='Book Title',
+    )
+    subtitle = forms.CharField(
+        widget=forms.TextInput,
+        required=False,
+    )
+    author = forms.CharField(
+        widget=forms.TextInput,
+        required=True,
+        label='Submitting Author/Editor',
+    )
 
 
 class DefaultNotRequiredForm(forms.Form):
-    title = forms.CharField(widget=forms.TextInput, required=False,
-                            label='Book Title')
-    subtitle = forms.CharField(widget=forms.TextInput, required=False)
-    author = forms.CharField(widget=forms.TextInput, required=False,
-                             label='Submitting Author/Editor')
+    title = forms.CharField(
+        widget=forms.TextInput,
+        required=False,
+        label='Book Title',
+    )
+    subtitle = forms.CharField(
+        widget=forms.TextInput,
+        required=False,
+    )
+    author = forms.CharField(
+        widget=forms.TextInput,
+        required=False,
+        label='Submitting Author/Editor',
+    )
 
 
 def render_choices(choices):
@@ -128,6 +172,7 @@ def render_choices(choices):
 
 
 class GeneratedForm(forms.Form):
+
     def __init__(self, *args, **kwargs):
 
         form_obj = kwargs.pop('form', None)
