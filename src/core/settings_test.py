6483129672425c17509cd25f21526f9787f4074b
Francesco de Virgilio <fradeve@inventati.org>
2018-11-11 20:02:34 +0000

    minor fixes to Tox

--- src/core/settings_test.py
+++ src/core/settings_test.py
@@ -1,8 +1,8 @@
 # ## GENERIC CONFIG ##
 
-SECRET_KEY = '_%@8*2$*1*i&um4+#a6w(%xqa_19=tfmhu9u-l*7t(a$g(2)wg'
-
 from .settings_dev import *
+
+SECRET_KEY = '_%@8*2$*1*i&um4+#a6w(%xqa_19=tfmhu9u-l*7t(a$g(2)wg'
 
 DEBUG = False
 DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
