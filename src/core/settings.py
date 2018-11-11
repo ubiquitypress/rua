bbef3551838a0c5cfda3683aa23ee17e7a259317
Francesco de Virgilio <fradeve@inventati.org>
2018-11-11 20:02:48 +0000

    Bump version: 3.0.0 → 3.0.1

--- src/core/settings.py
+++ src/core/settings.py
@@ -187,7 +187,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '3.0.0'
+RUA_VERSION = '3.0.1'
 
 
 # ## LOGGING ##
