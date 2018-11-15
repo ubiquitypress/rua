1bef04b11e0151d875a2ae5381ef02479f4b4ab4
Francesco de Virgilio <fradeve@inventati.org>
2018-11-15 22:22:51 +0000

    changed test pgsql credentials

--- src/core/settings_test.py
+++ src/core/settings_test.py
@@ -1,8 +1,21 @@
-# ## GENERIC CONFIG ##
-
 from .settings_dev import *
 
+
 SECRET_KEY = '_%@8*2$*1*i&um4+#a6w(%xqa_19=tfmhu9u-l*7t(a$g(2)wg'
+
+
+# ## DATABASE ##
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DATABASE_NAME', 'postgres'),
+        'USER': os.getenv('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.getenv('DATABASE_PASS', 'postgres'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),
+        'PORT': os.getenv('DATABASE_PORT', '5432')
+    }
+}
 
 DEBUG = False
 DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
