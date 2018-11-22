521fdaac730201c8af846c2712122e6603da8c35
Simon Crowe <info@simoncrowe.net>
2018-11-22 10:48:19 +0000

    Bump version: 3.0.13 → 3.0.14

--- src/core/settings.py
+++ src/core/settings.py
@@ -189,7 +189,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '3.0.13'
+RUA_VERSION = '3.0.14'
 
 
 # ## LOGGING ##
