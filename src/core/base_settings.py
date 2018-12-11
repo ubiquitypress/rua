f2c003377271a29a60acbf98f1c36c461088cc92
Simon Crowe <info@simoncrowe.net>
2018-12-11 10:53:37 +0000

    Bump version: 1.9.13 → 1.9.14

--- src/core/base_settings.py
+++ src/core/base_settings.py
@@ -178,7 +178,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '1.9.13'
+RUA_VERSION = '1.9.14'
 
 
 # ## EXTERNAL SERVICES ##
